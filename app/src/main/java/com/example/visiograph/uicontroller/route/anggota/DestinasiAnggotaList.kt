99f3c596a9bae6355b8bf7250ffda7b715c8a428
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-01-15 18:18:49 +0700

    feat: menambahkan destinasi list anggota

---
+++ app/src/main/java/com/example/visiograph/uicontroller/route/anggota/DestinasiAnggotaList.kt
@@ -0,0 +1,9 @@
+package com.example.visiograph.uicontroller.route.anggota
+
+import com.example.visiograph.R
+import com.example.visiograph.uicontroller.route.DestinasiNavigasi
+
+object DestinasiAnggotaList: DestinasiNavigasi {
+    override val route = "list_anggota"
+    override val titleRes = R.string.app_name
+}
