d5ae4977b40f93544268b311b5d0e646fb182046
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-01-15 18:20:19 +0700

    feat: menambahkan destinasi login

---
+++ app/src/main/java/com/example/visiograph/uicontroller/route/DestinasiLogin.kt
@@ -0,0 +1,8 @@
+package com.example.visiograph.uicontroller.route
+
+import com.example.visiograph.R
+
+object DestinasiLogin : DestinasiNavigasi {
+    override val route = "login"
+    override val titleRes = R.string.login
+}
